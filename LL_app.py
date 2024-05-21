02a9683c633134960957b257e7197f88c8d8d86e
zeqkik <ezequielcalmeida@gmail.com>
2024-05-20 21:43:36 -0300

    Creation of the classes for Node and LinkedList

---
+++ LL_app.py
@@ -0,0 +1,5 @@
+import LinkedList
+
+my_linked_list = LinkedList(4)
+
+print(my_linked_list.head.value)
---
+++ LinkedList.py
@@ -0,0 +1,10 @@
+import Node
+
+class LinkedList:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+        
