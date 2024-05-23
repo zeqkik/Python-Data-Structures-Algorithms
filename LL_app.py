f5105cb6d19e20d9a12cfb7f8397af1c2de1beee
zeqkik <ezequielcalmeida@gmail.com>
2024-05-22 22:59:56 -0300

    Adding print_list append and pop methods to LL

--- LL_app.py
+++ LL_app.py
@@ -1,5 +1,24 @@
-import LinkedList
+##
+
+from LinkedList import LinkedList
+
+###### Question 1 - Constructor ######
 
 my_linked_list = LinkedList(4)
+print("Question 1 - Constructor")
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length)
+print("\n")
 
-print(my_linked_list.head.value)
+###### Question 2 - Append ######
+print("Question 2 - Constructor")
+my_linked_list.append(2)
+my_linked_list.print_list()
+print("\n")
+
+### Question 3 - Pop ###
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print(my_linked_list.pop())
+print("\n")
--- LinkedList.py
+++ LinkedList.py
@@ -1,4 +1,4 @@
-import Node
+from Node import Node
 
 class LinkedList:
     def __init__(self,value):
@@ -7,4 +7,38 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-        
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if(self.head is None):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node #last node point to the new node
+            self.tail = new_node
+        self.length+=1
+        return True 
+    
+    def pop(self):
+        if(self.length == 0):
+            return None
+        elif(self.length == 1):
+            temp = self.tail
+            self.head = None
+            self.tail = None
+            self.length -=1
+            return temp
+        else:
+            temp = self.head
+            while(temp.next):
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+        self.length -=1
+        return temp
